3a81744ed853b131c0317a6a4ba28f9246d1f801
StudyingDragon <amanda.e.farmer@gmail.com>
2026-06-29 15:04:14 -0400

    Created Player class. Updated auxillary files.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame # pyright: ignore[reportMissingImports]
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main() -> None:
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0.0
 
     while True:
@@ -17,6 +19,7 @@ def main() -> None:
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
